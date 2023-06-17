7c5a9b244254f256d9c2248e56f7bf5926a68d83
HL2I0O <hanbo0lin@gmail.com>
2023-06-17 14:21:20 -0400

    Subset data to columns of relevance
    Columns to keep from Covid Deaths:
    -Location
    -Date
    -Total_Cases
    -New_Cases
    -Total_Deaths
    -Population

--- SQL_Data_Exploration.sql
+++ SQL_Data_Exploration.sql
@@ -9,7 +9,13 @@ select * from coviddeaths limit 10;
 select * from covidvaccinations limit 10;
 
 #Query 1: Test query to make sure data is imported correctly:
-select * from coviddeaths order by 3,4
+select * from coviddeaths order by 3,4;
 
+select * from covidvaccinations order by 3,4;
+
+#Query 2- Subset to data we are using:
+select Location, date, total_cases, new_cases, total_deaths, population
+from coviddeaths
+order by 1,2;
 
 #
